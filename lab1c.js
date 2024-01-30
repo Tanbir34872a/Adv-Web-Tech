88ca8e106165f7a6e166feb6da131f8543fa70b8
Tanbir Yousuf <yousuf.tanbir@gmail.com>
2024-01-30 13:19:24 +0600

    Add

--- lab1c.js
+++ lab1c.js
@@ -2,3 +2,9 @@ function greet(name){
     console.log(`Hello ${name}!`);
 }
 greet('Tanbir');
+
+function sum(a, b){
+    return a + b;
+}
+let result  = sum(2,7);
+console.log(result);
---
+++ lab1d.js
@@ -0,0 +1,7 @@
+let greet = name => console.log(`Hello ${name}!`);
+greet('Tanbir');
+
+const hello = function(name){
+    console.log(`Hello ${name}!`);
+};
+hello("Abbas");
